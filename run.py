ff1ae03f7b8a51dc1eb5efde009c2852caca68ed
송민석 <heroicms@g.skku.edu>
2024-07-10 09:11:50 +0900

    feat : window version

--- run.py
+++ run.py
@@ -6,11 +6,18 @@ import sys
 
 # 하이퍼파라미터 설정
 
-lineK = 0.005
-angleK = 18
+angleK = 18 # 회전 각도 배율 
 
-lineR = 470
+lineR = 470 # 기본 라인 위치 설정
 lineL = 170
+
+RMax = 520 # 우측 임계값 (좌측 차선) -> 좌측 차선에 민감하게 반응하면 올리고 둔감하면 내리기
+RMin = 420 # 좌측 임계값 (우측 차선) -> 우측 차선에 민감하게 반응하면 내리고 둔감하면 올리기
+
+laneMove = 0.25 # 라인에서 벗어났을 때 움직이는 정도
+laneC = 20 # 라인 보정치 -> 웬만하면 건들지 말고 라인 보정 이후에 정신 못차리면 수정(lane Move 크게 바꾸면 비례해서 수정 추천)
+
+Svalue = 50 # 색 구분값 초록색 인식되면 내리고 흰색 안보이면 올리기
 
 class ComputerVision(object):
     def grayscale(self, img):
@@ -23,7 +30,7 @@ class ComputerVision(object):
         cv2.imshow("V", imgV)
         """
         lower = np.array([0, 0, 150])
-        upper = np.array([255, 50, 255])
+        upper = np.array([255, Svalue, 255])
 
         mask = cv2.inRange(hsv, lower, upper)
 
@@ -76,17 +83,17 @@ class ComputerVision(object):
         
         global lineL, lineR
 
-        if lineR > 520:
-            lineL = 170
-            lineR = 470
-            return -1
-        if lineR < 420:
-            lineL = 170
-            lineR = 470
-            return 0
+        if lineR > RMax:
+            lineL -= laneC
+            lineR -= laneC
+            return min(-1 * laneMove, result)
+        if lineR < Rmin:
+            lineL += laneC
+            lineR += laneC
+            return max(laneMove, result)
         
         if cnt == 0:
-            return None
+            return -999
         
         result = total / cnt
 
@@ -111,10 +118,10 @@ class ComputerVision(object):
     def plothistogram(self, image):
         global lineL, lineR
         histogram = np.sum(image[7 * image.shape[0]//8:, :], axis=0)
-        indices = np.where(histogram >= 10000)[0]
+        indices = np.where(histogram >= 7000)[0]
         if indices.size > 0:
-            tempR = indices[np.argmax(np.abs(indices - lineR))]
-            tempL = indices[np.argmax(np.abs(indices - lineL))]
+            tempR = indices[np.argmin(np.abs(indices - lineR))]
+            tempL = indices[np.argmin(np.abs(indices - lineL))]
             if np.abs(tempR - lineR) < 30:
                 a = tempR - lineR
                 lineR += a
@@ -162,7 +169,7 @@ class ComputerVision(object):
         dst = self.morphology(hist, (2, 2), mode="opening")
 
         # 가우시안 블러 적용
-        #cv2.imshow("dst", dst)
+        cv2.imshow("dst", dst)
         imshape = img.shape
         vertices = np.array([[
             (160, imshape[0] * 1.0),          
@@ -182,16 +189,17 @@ class ComputerVision(object):
 class Arduino:
     def __init__(self):
         # 초기 상태 설정
-        self.cap = cv2.VideoCapture("/dev/video2")
+        self.cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
         self.cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-        self.arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+        self.arduino = serial.Serial(port='COM5', baudrate=9600, timeout=.1)
         time.sleep(2.0)
         self.computerVision = ComputerVision()
 
     def __delete__(self):
         self.cap.release()
+        self.arduino.release()
 
     def move(self):
         # 모터 회전 후 이동
@@ -202,32 +210,25 @@ class Arduino:
                 self.reward = self.take_picture()
                 
                 if self.reward is None:
+                    print("Camera Disconnected")
+                    angle = 999
+                elif self.reward == -999:
+                    print("No Line")
                     continue
-                
-                angle = self.reward * angleK + 17
+                else:
+                    angle = self.reward * angleK + 17
                                 
                 angle = int(angle)
-                #print(lineL, lineR, angle)
-                """
-                if angle > 27:
-                    check = 1
-                elif angle < 13:
-                    check = -1
-                elif 19 < angle < 21:
-                    if check == 1:
-                        angle = 10
-                        check = 0
-                    elif check == -1:
-                        angle = 30
-                        chekc = 0
-                """
+                print(lineL, lineR, angle)
+                
                 end = time.time()
                 if end - start < 0.1:
                     time.sleep(0.1 - (end - start))
                 self.arduino.flush()
                 self.arduino.write(f"{angle}\n".encode('utf-8'))
-                #cv2.waitKey(1)
-                
+                cv2.waitKey(1)
+        except serial.SerialException as e:
+            print(f"Serial Error : {e}")
         except KeyboardInterrupt:
             print("stop")
         finally:
