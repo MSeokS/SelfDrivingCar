ebaabb9637b133bddaa96f4f1723f4f40ba02e7d
hogyeomchu <gyeom0808@gmail.com>
2024-07-16 15:02:48 +0900

    feat : Parking

---
+++ LiDAR.py
@@ -0,0 +1,69 @@
+# LiDAR Lib
+import Lib_LiDAR as LiDAR
+import serial
+import time
+import numpy as np
+
+arduino = serial.Serial(port='COM5', baudrate=9600, timeout=.1)
+time.sleep(2)  # 아두이노 초기화 시간 대기
+
+if __name__ == "__main__":
+    env = LiDAR.libLidar('COM3')
+    env.init()
+    count = 0
+    arduino.write("0\n".encode('utf-8'))
+
+    for scan in env.scanning():
+        scan1 = env.getAngleDistanceRange(scan, 75, 80, 500, 2000)
+
+        if len(scan1) > 0:
+            print(scan1)
+            print('stop1')
+            time.sleep(2)
+            break
+        else:
+            print('go')
+        if count == 600:
+            env.stop()
+            break
+
+        count += 1
+
+    
+    count = 0
+    for scan in env.scanning():
+        scan2 = env.getAngleDistanceRange(scan, 75, 80, 500, 2000)
+
+        if len(scan2) > 0:
+            print(scan2)
+            print('stop2')
+            arduino.write("1\n".encode('utf-8'))
+            time.sleep(5)
+            break
+        else:
+            print('go')
+        if count == 600:
+            env.stop()
+            break
+
+        count += 1
+
+    count = 0
+    for scan in env.scanning():
+        scan_l = env.getAngleDistanceRange(scan, 260, 270, 700, 1200)
+        scan_r = env.getAngleDistanceRange(scan, 90, 100, 700, 1200)
+        scan_comb = np.concatenate((scan_l, scan_r), axis=0)
+
+        if len(scan_comb) > 0:
+            print(scan_comb)
+            print('stop3')
+            env.stop()
+            arduino.write("2\n".encode('utf-8'))
+            break
+        else:
+            print('go')
+        if count == 600:
+            env.stop()
+            break
+
+        count += 1
---
+++ Parking/Parking.ino
@@ -0,0 +1,103 @@
+#include <Car_Library.h>
+
+int analogPin = A6;
+int motorA1 = 2;    // 모터 드라이버 IN1
+int motorA2 = 3;    // 모터 드라이버 IN2
+int motorB1 = 4;
+int motorB2 = 5;
+int motorH1 = 6;
+int motorH2 = 7;
+int reg;
+int temp;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);       // 시리얼 통신 시작, 통신 속도 설정
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2, OUTPUT);
+  pinMode(motorB1, OUTPUT);
+  pinMode(motorB2, OUTPUT);
+  pinMode(analogPin, INPUT);
+  
+  reg = potentiometer_Read(analogPin);
+  if(reg < 17) {
+    while(reg < 17) {
+      motor_forward(motorH1, motorH2, 150);
+      delay(10);
+      motor_hold(motorH1, motorH2);
+      reg = potentiometer_Read(analogPin);
+    }
+  }
+  else if(reg > 17) {
+    while(reg > 17) {
+      motor_backward(motorH1, motorH2, 150);
+      delay(10);
+      motor_hold(motorH1, motorH2);
+      reg = potentiometer_Read(analogPin);
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) { // 시리얼 버퍼에 데이터가 있는지 확인
+    temp = Serial.parseInt(); // 정수형 데이터 읽기
+    Serial.print("Received Value: ");
+    Serial.println(temp); // 받은 값 출력(라이다)
+
+    if(temp == 0) {
+      motor_forward(motorA1, motorA2, 100);
+      motor_forward(motorB1, motorB2, 100);
+    }
+    
+    else if(temp == 1) {
+      motor_hold(motorA1, motorA2);
+      motor_hold(motorB1, motorB2); // 멈추고
+      motor_backward(motorH1, motorH2, 150); // 오른쪽으로 핸들을 돌림
+      delay(1000);
+      motor_hold(motorH1, motorH2);
+      motor_backward(motorA1, motorA2, 100); // 회전
+      motor_forward(motorB1, motorB2, 100);
+      delay(10000);
+      
+      reg = potentiometer_Read(analogPin);
+      while(reg < 17) {
+        motor_forward(motorH1, motorH2, 150);
+        delay(10);
+        motor_hold(motorH1, motorH2);
+        reg = potentiometer_Read(analogPin);
+      }
+      
+      
+      motor_backward(motorA1, motorA2, 100); // 후진
+      motor_backward(motorB1, motorB2, 100);
+      delay(200);
+      temp = 2;
+    }
+
+    else if(temp == 2) {
+      motor_hold(motorA1, motorA2);
+      motor_hold(motorB1, motorB2);
+      temp = 3;
+    }
+
+    else if(temp == 3) {
+      while(reg > 10) {
+        motor_backward(motorH1, motorH2, 150);
+        delay(10);
+        motor_hold(motorH1, motorH2);
+        reg = potentiometer_Read(analogPin);
+      }
+
+      motor_forward(motorA1, motorA2, 100);
+      motor_forward(motorB1, motorB2, 100);
+      delay(10000);
+      temp = 2;
+    }
+
+    while(Serial.available() > 0) {
+      Serial.read();
+    }
+  }
+}
